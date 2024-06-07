9baeca5d32b36767fb294dae46f1d24e454f8780
Raquel <raquelsglez99@gmail.com>
2024-06-07 19:02:30 +0200

    feat: Change position file index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 const express = require('express')
 const dotenv = require('dotenv');
 const app = express();
-const { dbConnection } = require('./src/config/db');
+const { dbConnection } = require('./config/db');
 const PORT = process.env.PORT;
 const methodOverride = require('method-override');
-const productRoutes = require('./src/routes/productRoutes');
-//const path = require('path');
+const productRoutes = require('./routes/productRoutes');
 
 dotenv.config();
+//cargar variables de entorno del archivo .env
 app.use(methodOverride('_method'));
  //necesario cuando se utiliza formularios HTML para enviar solicitudes PUT y DELETE. 
 //Este middleware permite usar métodos HTTP PUT y DELETE en lugares donde normalmente solo se permiten GET y POST
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('public')); //// Servir archivos estáticos desde la carpeta 'public'
 
 app.use('/', productRoutes);
 
